fe887a0dd0d30114b0f657cb639d3ef3aa344e4d
KircheisHe <hechenzhang@outlook.com>
2017-01-10 09:49:04 -0800

    Complete Task

--- ShoppingCart/EmojiSelectionViewController.swift
+++ ShoppingCart/EmojiSelectionViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class EmojiSelectionViewController: UIViewController {
     
+    @IBAction func HitSaveButton(_ sender: UIButton) {
+        let leftText = LeftText.text
+        let rightText = RightText.text
+        
+        if (leftText != nil && rightText != nil) {
+            emojiDelegate?.create(emojiGroup: (leftText!, rightText!))
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBOutlet weak var LeftText: UITextField!
+    @IBOutlet weak var RightText: UITextField!
+    
+    var emojiDelegate : EmojiCreation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red:0.22, green:0.33, blue:0.58, alpha:1.00)
