e6fc4152b4e1353f90198080a98affee751a0daf
Ryan Hennings <r.hennings11@gmail.com>
2017-02-04 18:10:31 -0800

    Shopping Cart Lab complete

--- ShoppingCart/EmojiSelectionViewController.swift
+++ ShoppingCart/EmojiSelectionViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class EmojiSelectionViewController: UIViewController {
     
+    @IBOutlet weak var emoji1Field: UITextField!
+    @IBOutlet weak var emoj2Field: UITextField!
+    
+    var emojiDelegate: EmojiCreation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red:0.22, green:0.33, blue:0.58, alpha:1.00)
     }
     
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        let e1 = emoji1Field.text
+        let e2 = emoj2Field.text
+        
+        if e1 != nil && e2 != nil {
+            print("RYAN: Sending emojis to create()")
+            let mojisTup = (e1!, e2!)
+            emojiDelegate?.create(emojiGroup: mojisTup)
+            dismiss(animated: true, completion: nil)
+        }
+        
+    }
 }
