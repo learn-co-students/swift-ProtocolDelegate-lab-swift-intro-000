9b8bd86176f85effb9dbde841f6a4734f05a1e97
Kelly Lougheed <kellylougheed@gmail.com>
2017-05-01 10:28:50 -0700

    Add answers

--- ShoppingCart/EmojiSelectionViewController.swift
+++ ShoppingCart/EmojiSelectionViewController.swift
@@ -15,4 +15,15 @@ class EmojiSelectionViewController: UIViewController {
         view.backgroundColor = UIColor(red:0.22, green:0.33, blue:0.58, alpha:1.00)
     }
     
+    var emojiDelegate: EmojiCreation?
+    
+    @IBOutlet weak var field1: UITextField!
+    @IBOutlet weak var field2: UITextField!
+    
+    @IBAction func save(_ sender: Any) {
+        let emoji1 = field1.text
+        let emoji2 = field2.text
+        emojiDelegate?.create(emojiGroup: (emoji1!, emoji2!))
+        dismiss(animated: true, completion: nil)
+    }
 }
