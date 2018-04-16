7abf1026905557e4229a561e07788113d8dafb6e
MlleDolce <andreakchristie@gmail.com>
2018-04-16 12:38:09 -0400

    Completed Shopping Cart Lab.

--- ShoppingCart/ShoppingListViewController.swift
+++ ShoppingCart/ShoppingListViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-
 // TODO: Create protocol here.
-
+protocol EmojiCreation {
+    func create(emojiGroup: (String, String))
+}
 
 class ShoppingViewController: UIViewController {
     
@@ -25,6 +26,11 @@ class ShoppingViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.allowsSelection = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! EmojiSelectionViewController
+        destVC.emojiDelegate = self
     }
     
 }
@@ -48,6 +54,11 @@ extension ShoppingViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate Methods
 extension ShoppingViewController: UITableViewDelegate { }
 
-
+extension ShoppingViewController: EmojiCreation {
+    func create(emojiGroup: (String, String)) {
+        emojis.append(emojiGroup)
+        tableView.reloadData()
+    }
+}
 
 
