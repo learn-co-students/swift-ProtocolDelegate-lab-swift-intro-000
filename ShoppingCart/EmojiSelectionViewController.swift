64ad28bb007f783f2cadac1025a879a59fd885f4
nalamoudi <nahoorii_91@hotmail.com>
2018-10-18 14:28:41 +0300

    done protocol delegates

--- ShoppingCart/EmojiSelectionViewController.swift
+++ ShoppingCart/EmojiSelectionViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class EmojiSelectionViewController: UIViewController {
     
+    var emojiDelegate: EmojiCreation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red:0.22, green:0.33, blue:0.58, alpha:1.00)
     }
+    
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+        var text1: String = ""
+        var text2: String = ""
+        
+        
+        
+        if let x1 = textField1.text {
+            text1 = x1
+        }
+        if let x2 = textField2.text {
+            text2 = x2
+        }
+        let tuple: (String, String) = (text1, text2)
+        
+        emojiDelegate?.create(emojiGroup: tuple)
+        
+        dismiss(animated: true, completion: nil)
+ 
+        
+        
+    }
+    
     
 }
